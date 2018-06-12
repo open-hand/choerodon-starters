a878c412687939d6f43572ce17c305f64b064552
superlee <superleader8@gmail.com>
2018-06-12 17:13:58 +0800

    [ADD] 添加initRoleCode常量类 [DEL] 删除permission注解的roles默认值

---
+++ choerodon-starter-core/src/main/java/io/choerodon/core/iam/InitRoleCode.java
@@ -0,0 +1,50 @@
+package io.choerodon.core.iam;
+
+
+/**
+ * 初始化角色的code
+ * @author superlee
+ */
+public class InitRoleCode {
+
+    /**
+     * 全局层管理员
+     */
+    public static final String SITE_ADMINISTRATOR = "role/site/default/administrator";
+
+    /**
+     * 组织层管理员
+     */
+    public static final String ORGANIZATION_ADMINISTRATOR = "role/organization/default/administrator";
+
+    /**
+     * 项目层管理员
+     */
+    public static final String PROJECT_ADMINISTRATOR = "role/project/default/administrator";
+
+    /**
+     * 部署管理员
+     */
+    public static final String DEPLOY_ADMINISTRATOR = "role/project/default/deploy-administrator";
+
+    /**
+     * 项目成员
+     */
+    public static final String PROJECT_MEMBER = "role/project/default/project-member";
+
+    /**
+     * 项目所有者
+     */
+    public static final String PROJECT_OWNER = "role/project/default/project-owner";
+
+    public static String[] values () {
+       return new String[] {
+                SITE_ADMINISTRATOR,
+                ORGANIZATION_ADMINISTRATOR,
+                PROJECT_ADMINISTRATOR,
+                DEPLOY_ADMINISTRATOR,
+                PROJECT_MEMBER,
+                PROJECT_OWNER
+        };
+    }
+}
