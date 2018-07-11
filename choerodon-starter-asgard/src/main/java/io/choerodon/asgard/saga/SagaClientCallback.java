39b76f79f87d6d0261c7fc62df51d17d1b57b59d
flyleft <jcalaz@163.com>
2018-07-11 09:43:35 +0800

    [IMP] 修改了SagaClientCallback

--- choerodon-starter-asgard/src/main/java/io/choerodon/asgard/saga/SagaClientCallback.java
+++ choerodon-starter-asgard/src/main/java/io/choerodon/asgard/saga/SagaClientCallback.java
@@ -3,11 +3,12 @@ package io.choerodon.asgard.saga;
 import io.choerodon.core.exception.CommonException;
 
 import java.util.List;
+import java.util.Set;
 
 public class SagaClientCallback implements SagaClient {
 
     @Override
-    public List<DataObject.SagaTaskInstanceDTO> pollBatch(String code, String instance, List<Long> filterIds) {
+    public List<DataObject.SagaTaskInstanceDTO> pollBatch(String code, String instance, Set<Long> filterIds) {
         throw new CommonException("error.sagaTaskInstance.poll");
     }
 
