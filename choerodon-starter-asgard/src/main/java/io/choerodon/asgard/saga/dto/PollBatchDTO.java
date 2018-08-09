dfd6b85fd3abc0507e6a37c358e553a0308c58f4
flyleft <jcalaz@163.com>
2018-08-09 14:46:30 +0800

    [IMP] 优化saga日志打印

--- choerodon-starter-asgard/src/main/java/io/choerodon/asgard/saga/dto/PollBatchDTO.java
+++ choerodon-starter-asgard/src/main/java/io/choerodon/asgard/saga/dto/PollBatchDTO.java
@@ -1,9 +1,6 @@
 package io.choerodon.asgard.saga.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
@@ -13,6 +10,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class PollBatchDTO {
 
     @NotEmpty
