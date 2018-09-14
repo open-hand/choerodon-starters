a1a12d28a94fbf35d596019bbd487e9b3fb09e75
Eugen <shiying.shen@hand-china.com>
2018-09-14 10:58:17 +0800

    [IMP]asgard任务调度jobParam添加类型转换

--- choerodon-starter-asgard/src/main/java/io/choerodon/asgard/property/PropertyJobParam.java
+++ choerodon-starter-asgard/src/main/java/io/choerodon/asgard/property/PropertyJobParam.java
@@ -1,31 +1,43 @@
 package io.choerodon.asgard.property;
 
-import io.choerodon.asgard.schedule.annotation.JobParam;
-import io.choerodon.asgard.schedule.ParamType;
-import io.choerodon.asgard.schedule.exception.NotSupportParamTypeException;
+import java.lang.reflect.InvocationTargetException;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ValueConstants;
+
+import io.choerodon.asgard.saga.SagaMonitor;
+import io.choerodon.asgard.schedule.ParamType;
+import io.choerodon.asgard.schedule.annotation.JobParam;
+import io.choerodon.asgard.schedule.exception.JobParamDefaultValueParseException;
+import io.choerodon.asgard.schedule.exception.NotSupportParamTypeException;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class PropertyJobParam {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SagaMonitor.class);
 
     private String name;
 
-    private String defaultValue;
+    private Object defaultValue;
 
     private String type;
 
     public PropertyJobParam(final JobParam jobParam) {
         this.name = jobParam.name();
-        if (!ValueConstants.DEFAULT_NONE.equals(jobParam.defaultValue())) {
-            this.defaultValue = jobParam.defaultValue();
-        }
-
         this.type = getParamTypeByClass(jobParam.type()).getValue();
+        if (!ValueConstants.DEFAULT_NONE.equals(jobParam.defaultValue())) {
+            try {
+                this.defaultValue = jobParam.type().getConstructor(String.class).newInstance(jobParam.defaultValue());
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                throw new JobParamDefaultValueParseException(e, jobParam.type(), jobParam.defaultValue());
+            }
+
+        }
     }
 
     private ParamType getParamTypeByClass(final Class<?> claz) {
