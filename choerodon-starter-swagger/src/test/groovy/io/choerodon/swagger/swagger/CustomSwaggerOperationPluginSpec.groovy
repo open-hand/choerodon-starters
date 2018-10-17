95ea1a2910adf0f8223d3ef7811c58f123e7be13
LongHe1996 <LongHe1996@foxmail.com>
2018-10-17 15:33:40 +0800

    [ADD]添加Swagger的单元测试（本地89%） 去掉hitoa oauthResource的cglib依赖

---
+++ choerodon-starter-swagger/src/test/groovy/io/choerodon/swagger/swagger/CustomSwaggerOperationPluginSpec.groovy
@@ -0,0 +1,43 @@
+package io.choerodon.swagger.swagger
+
+import com.google.common.base.Optional
+import io.choerodon.core.iam.ResourceLevel
+import io.choerodon.swagger.annotation.Label
+import io.choerodon.swagger.annotation.Permission
+import spock.lang.Specification
+import springfox.documentation.builders.OperationBuilder
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.OperationContext
+
+class CustomSwaggerOperationPluginSpec extends Specification {
+    private CustomSwaggerOperationPlugin customSwaggerOperationPlugin = new CustomSwaggerOperationPlugin()
+    Optional<Permission> optional1 = Mock(Optional)
+    Optional<Permission> optional2 = Mock(Optional)
+
+    def "Apply"() {
+        given: "mock参数准备"
+        def permission = Mock(Permission)
+        permission.level() >> { return ResourceLevel.SITE }
+        def label = Mock(Label)
+        optional1.orNull() >> { permission }
+        optional2.orNull() >> { label }
+
+        and: "参数准备"
+        def context = Mock(OperationContext)
+        context.findAnnotation(Permission.class) >> { return optional1 }
+        context.findAnnotation(Label.class) >> { return optional2 }
+        context.getName() >> { return "name" }
+        context.operationBuilder() >> { return Mock(OperationBuilder) }
+        when: "方法调用"
+        customSwaggerOperationPlugin.apply(context)
+        then: "无异常抛出"
+        noExceptionThrown()
+    }
+
+    def "Supports"() {
+        when: "方法调用"
+        def supports = customSwaggerOperationPlugin.supports(Mock(DocumentationType))
+        then: "返回true"
+        supports == true
+    }
+}
