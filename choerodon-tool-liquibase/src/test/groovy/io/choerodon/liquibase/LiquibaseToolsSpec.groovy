641968a89aa23b735f54833314aa3df872ebe05e
zmf <1984654893@qq.com>
2018-10-22 19:18:02 +0800

    [ADD] local coverage of unit tests for choerodon-tool-liquibase comes 90%

--- choerodon-tool-liquibase/src/test/groovy/io/choerodon/liquibase/LiquibaseToolsSpec.groovy
+++ choerodon-tool-liquibase/src/test/groovy/io/choerodon/liquibase/LiquibaseToolsSpec.groovy
@@ -10,18 +10,9 @@ import spock.lang.Stepwise
  *
  */
 @Stepwise
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = LiquibaseTools.class)
 class LiquibaseToolsSpec extends Specification {
-//    @Autowired
-//    ApplicationContext applicationContext
-
     LiquibaseExecutor liquibaseExecutor = Mock(LiquibaseExecutor)
 
-
-    void setup() {
-//        StartupRunner startupRunner = applicationContext.getBean(StartupRunner)
-//        startupRunner.liquibaseExecutor = Mock(LiquibaseExecutor)
-    }
 
     def main(String... values) {
         println("tttttttttttttttttttttttttttttt")
@@ -31,46 +22,33 @@ class LiquibaseToolsSpec extends Specification {
         when:
         LiquibaseTools.main()
 
-
         then: "校验结果"
-        thrown(Exception)
-        1 * liquibaseExecutor.execute() >> { true }
+        noExceptionThrown()
     }
 
     def "Startup runner 1"() {
         given: "配置环境"
         StartupRunner sr = new StartupRunner()
         sr.liquibaseExecutor = liquibaseExecutor
-
+        liquibaseExecutor.execute() >> { false }
 
         when: "调用方法"
         sr.run()
 
-
         then: "结果分析"
         noExceptionThrown()
-
-        1 * liquibaseExecutor.execute(_) >> { false }
-//        when:
-//        LiquibaseTools.main("")
-//
-//        then:
-//        noExceptionThrown()
     }
 
     def "Startup runner 0"() {
         given: "配置环境"
         StartupRunner sr = new StartupRunner()
         sr.liquibaseExecutor = liquibaseExecutor
-
+        liquibaseExecutor.execute() >> { false }
 
         when: "调用方法"
         sr.run()
 
-
         then: "结果分析"
         noExceptionThrown()
-
-        1 * liquibaseExecutor.execute(_) >> { false }
     }
 }
