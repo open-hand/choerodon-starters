e4f46a953884e1cbe8b96d48e7ee2c33a23045de
superlee <superleader8@gmail.com>
2018-11-23 10:47:30 +0800

    [ADD] 添加BasePasswordPolicyMapper

---
+++ choerodon-starter-oauth-core/src/main/java/io/choerodon/oauth/core/password/mapper/BasePasswordPolicyMapper.java
@@ -0,0 +1,10 @@
+package io.choerodon.oauth.core.password.mapper;
+
+import io.choerodon.mybatis.common.BaseMapper;
+import io.choerodon.oauth.core.password.domain.BasePasswordPolicyDO;
+
+/**
+ * @author wuguokai
+ */
+public interface BasePasswordPolicyMapper extends BaseMapper<BasePasswordPolicyDO> {
+}
