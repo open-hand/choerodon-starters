7e1f9afd3d584c49a8e2985619a2f33a8b60de2a
xausky <xausky@163.com>
2019-03-29 09:21:30 +0800

    [ADD] 为freemarker模块添加测试

---
+++ choerodon-starter-freemarker/src/test/java/io/choerodon/freemarker/TestFreemarkerBean.java
@@ -0,0 +1,9 @@
+package io.choerodon.freemarker;
+
+import io.choerodon.freemarker.annotation.FreeMarkerBean;
+import org.springframework.stereotype.Component;
+
+@Component
+@FreeMarkerBean
+public class TestFreemarkerBean {
+}
