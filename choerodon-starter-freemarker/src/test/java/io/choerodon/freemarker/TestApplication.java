d73f528ce60cff9e5306e01a9ca573636e326638
xausky <xausky@163.com>
2019-03-29 09:21:30 +0800

    [ADD] 为freemarker模块添加测试

---
+++ choerodon-starter-freemarker/src/test/java/io/choerodon/freemarker/TestApplication.java
@@ -0,0 +1,12 @@
+package io.choerodon.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
