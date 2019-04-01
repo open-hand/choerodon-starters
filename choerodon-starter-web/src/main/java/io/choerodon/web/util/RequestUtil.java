aaa233d5cba6b7e871c71e52f41204f132f6f409
曾强 <qiang.zeng@hand-china.com>
2019-04-01 16:26:38 +0800

    [IMP] 提取web基础模块到choerodon

---
+++ choerodon-starter-web/src/main/java/io/choerodon/web/util/RequestUtil.java
@@ -0,0 +1,50 @@
+package io.choerodon.web.util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author njq.niu@hand-china.com
+ * @since 2016年1月19日
+ */
+public final class RequestUtil {
+
+    public static final String X_REQUESTED_WIDTH = "X-Requested-With";
+    public static final String XML_HTTP_REQUEST = "XMLHttpRequest";
+
+    private RequestUtil() {
+    }
+
+    /**
+     * 判断是否ajax请求.
+     *
+     * @param request HttpServletRequest
+     * @return 是否ajax请求.
+     */
+    public static boolean isAjaxRequest(HttpServletRequest request) {
+        String xr = request.getHeader(X_REQUESTED_WIDTH);
+        return (xr != null && XML_HTTP_REQUEST.equalsIgnoreCase(xr));
+    }
+
+    public static boolean isAPIRequest(HttpServletRequest request) {
+        return request.getRequestURI().contains("/api/");
+    }
+
+    public static boolean isMultipartRequest(HttpServletRequest request) {
+        String contentType = request.getContentType();
+        return contentType != null && contentType.toLowerCase().startsWith("multipart/");
+    }
+
+    public static String getCookieValue(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                String cookieName = cookie.getName();
+                if (cookieName.equals(name)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+}
