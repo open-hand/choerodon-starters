f45b778dba0deb57f2ac8836b9031447c7c858bd
xausky <xausky@163.com>
2019-04-02 10:27:10 +0800

    [DEL] 迁移dataset模块到modules

--- choerodon-starter-dataset/src/main/java/io/choerodon/dataset/DatasetAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package io.choerodon.dataset;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@ComponentScan
-@Configuration
-public class DatasetAutoConfiguration {
-}
