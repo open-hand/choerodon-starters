c2200605cc0817bcab20de15d06cbe49343b041d
xausky <xausky@163.com>
2019-04-18 15:19:12 +0800

    [FIX] 修复Actuator的权限问题和ApplicationContext问题

--- choerodon-starter-actuator/src/main/java/io/choerodon/actuator/endpoint/ActuatorEndpoint.java
+++ choerodon-starter-actuator/src/main/java/io/choerodon/actuator/endpoint/ActuatorEndpoint.java
@@ -2,6 +2,9 @@ package io.choerodon.actuator.endpoint;
 
 import io.choerodon.annotation.PermissionProcessor;
 import io.choerodon.annotation.entity.PermissionDescription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +15,19 @@ import java.util.Map;
 import java.util.TreeMap;
 
 @RestController
-@RequestMapping("/v1/actuator")
+@RequestMapping("/v2/choerodon/actuator")
 public class ActuatorEndpoint {
     private Map<String, PermissionDescription> permissions = new HashMap<>();
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @GetMapping("/{key}")
     private Map<String, Object> query(@PathVariable String key){
         Map<String, Object> result = new TreeMap<>();
         if ("permission".equals(key) || "all".equals(key)){
             if (permissions.isEmpty()){
-                PermissionProcessor.resolve(permissions);
+                PermissionProcessor.resolve(applicationContext.getBeansWithAnnotation(Controller.class), permissions);
             }
             result.put("permission", permissions);
         }
