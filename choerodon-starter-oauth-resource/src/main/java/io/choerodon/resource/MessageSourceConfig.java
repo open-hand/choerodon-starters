d00fcf8bfc00797227b6f264a05392adbbd8965d
曾强 <qiang.zeng@hand-china.com>
2019-04-24 11:37:53 +0800

    [DEL] MessageSourceConfig delete unnecessary import

--- choerodon-starter-oauth-resource/src/main/java/io/choerodon/resource/MessageSourceConfig.java
+++ choerodon-starter-oauth-resource/src/main/java/io/choerodon/resource/MessageSourceConfig.java
@@ -1,16 +1,13 @@
 package io.choerodon.resource;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 /**
+ * messageSource自动配置.
  *
- * messageSource自动配置
  * @author superlee
  * @since 2019-04-24
  */
