6240d6c2ac8cc438c4a19ca07a2c10fcea88e3c0
曾强 <qiang.zeng@hand-china.com>
2019-04-29 09:22:51 +0800

    [IMP] 基础异常类支持可变长参数

--- choerodon-starter-base/src/main/java/io/choerodon/base/exception/BaseRuntimeException.java
+++ choerodon-starter-base/src/main/java/io/choerodon/base/exception/BaseRuntimeException.java
@@ -2,7 +2,7 @@ package io.choerodon.base.exception;
 
 /**
  * @author xiangyu.qi@hand-china.com
- * @since 2018/3/27.
+ * @since 2018/3/27
  */
 public class BaseRuntimeException extends RuntimeException implements IBaseException {
 
@@ -13,6 +13,13 @@ public class BaseRuntimeException extends RuntimeException implements IBaseExcep
 
     private Object[] parameters;
 
+    public BaseRuntimeException(String code, Object... parameters) {
+        super(code);
+        this.code = code;
+        this.descriptionKey = code;
+        this.parameters = parameters;
+    }
+
     /**
      * 不应该直接实例化,应该定义子类.
      *
@@ -20,7 +27,7 @@ public class BaseRuntimeException extends RuntimeException implements IBaseExcep
      * @param descriptionKey 异常消息代码,系统描述中定义
      * @param parameters     如果没有参数,可以传 null
      */
-    protected BaseRuntimeException(String code, String descriptionKey, Object[] parameters) {
+    public BaseRuntimeException(String code, String descriptionKey, Object... parameters) {
         super(descriptionKey);
         this.code = code;
         this.descriptionKey = descriptionKey;
