72a1dbdb35cd096cdac0b89d5677d85128e38c98
azengqiang <qiang.zeng@hand-china.com>
2019-05-17 11:35:16 +0800

    [DEL] 删除BaseEntity

--- choerodon-starter-message/src/test/java/io/choerodon/message/TestEntityMonitor.java
+++ choerodon-starter-message/src/test/java/io/choerodon/message/TestEntityMonitor.java
@@ -1,8 +1,9 @@
 package io.choerodon.message;
 
-import io.choerodon.base.entity.BaseEntity;
+
 import io.choerodon.message.annotation.QueueMonitor;
 import io.choerodon.message.annotation.TopicMonitor;
+import io.choerodon.mybatis.entity.BaseDTO;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
@@ -10,23 +11,23 @@ import org.springframework.util.Assert;
 @Component
 @TopicMonitor(channel = "test:topicEntity")
 @QueueMonitor(queue = "test:queueEntity")
-public class TestEntityMonitor implements IQueueMessageListener<BaseEntity>, ITopicMessageListener<BaseEntity> {
+public class TestEntityMonitor implements IQueueMessageListener<BaseDTO>, ITopicMessageListener<BaseDTO> {
     public int topicCount = 0;
     public int queueCount = 0;
 
     @Override
-    public void onTopicMessage(BaseEntity message, String pattern) {
+    public void onTopicMessage(BaseDTO message, String pattern) {
         Assert.notNull(message, "message not should to null.");
         topicCount++;
     }
 
     @Override
-    public RedisSerializer<BaseEntity> getRedisSerializer() {
+    public RedisSerializer<BaseDTO> getRedisSerializer() {
         return null;
     }
 
     @Override
-    public void onQueueMessage(BaseEntity message, String queue) {
+    public void onQueueMessage(BaseDTO message, String queue) {
         Assert.notNull(message, "message not should to null.");
         queueCount++;
     }
