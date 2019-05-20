2016a2e5fdfee959c41af6453c20adc7aa5cd2ae
superlee <superleader8@gmail.com>
2019-05-20 20:37:42 +0800

    [ADD] 添加短信模版

--- choerodon-starter-core/src/main/java/io/choerodon/core/notify/PmTemplate.java
+++ choerodon-starter-core/src/main/java/io/choerodon/core/notify/PmTemplate.java
@@ -4,6 +4,7 @@ package io.choerodon.core.notify;
  * @author dengyouquan
  **/
 public interface PmTemplate extends NotifyTemplate {
+    @Override
     default String type(){
         return "pm";
     }
