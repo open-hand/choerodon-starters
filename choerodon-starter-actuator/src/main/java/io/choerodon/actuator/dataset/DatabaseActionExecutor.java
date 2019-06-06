3b4f9d008ee4faaaa1e2802f2279f59352fa1c60
xausky <xausky@163.com>
2019-06-06 16:48:02 +0800

    [DEL] 移除多余代码

--- choerodon-starter-actuator/src/main/java/io/choerodon/actuator/dataset/DatabaseActionExecutor.java
+++
@@ -1,7 +0,0 @@
-package io.choerodon.actuator.dataset;
-
-import io.choerodon.actuator.dataset.domain.DatabasePageAction;
-
-public interface DatabaseActionExecutor {
-    void process(DatabasePageAction action);
-}
