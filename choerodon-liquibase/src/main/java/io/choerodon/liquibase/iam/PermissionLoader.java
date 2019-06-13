5609c40f7cf9a7c63b6e348ab1ea77915f973e07
azengqiang <qiang.zeng@hand-china.com>
2019-06-13 19:19:57 +0800

    [IMP] 自动扫描权限 兼容oracle数据库

--- choerodon-liquibase/src/main/java/io/choerodon/liquibase/iam/PermissionLoader.java
+++ choerodon-liquibase/src/main/java/io/choerodon/liquibase/iam/PermissionLoader.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.choerodon.annotation.entity.PermissionDescription;
 import io.choerodon.annotation.entity.PermissionEntity;
 import io.choerodon.base.enums.ResourceType;
+import io.choerodon.liquibase.helper.LiquibaseHelper;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,7 +13,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -23,6 +23,11 @@ public class PermissionLoader {
     private static final char UNDERLINE = '-';
     private Connection connection;
     private Map<String, Long> roleMap;
+    private LiquibaseHelper liquibaseHelper;
+
+    public PermissionLoader(LiquibaseHelper liquibaseHelper) {
+        this.liquibaseHelper = liquibaseHelper;
+    }
 
     public void setServiceCode(String serviceCode) {
         this.serviceCode = serviceCode;
@@ -32,26 +37,27 @@ public class PermissionLoader {
         this.connection = connection;
         this.roleMap = queryRoleMap();
         ObjectMapper objectMapper = new ObjectMapper();
-        Map <String, PermissionDescription> descriptions = objectMapper.readValue(inputStream, objectMapper.getTypeFactory().constructMapType(HashMap.class, String.class, PermissionDescription.class));
-        for (Map.Entry<String, PermissionDescription> entry : descriptions.entrySet()){
+        Map<String, PermissionDescription> descriptions = objectMapper.readValue(inputStream, objectMapper.getTypeFactory().constructMapType(HashMap.class, String.class, PermissionDescription.class));
+        for (Map.Entry<String, PermissionDescription> entry : descriptions.entrySet()) {
             entry.getValue().setService(serviceCode);
             processDescription(entry.getKey(), entry.getValue());
         }
     }
+
     private void processDescription(String key, PermissionDescription description) throws SQLException {
         String[] names = key.split("\\.");
         String controllerClassName = names[names.length - 2];
         String action = names[names.length - 1];
         String resource = camelToUnderline(controllerClassName).replace("-controller", "");
 
-        if (description.getPermission() == null){
+        if (description.getPermission() == null) {
             PermissionEntity permissionEntity = new PermissionEntity();
             permissionEntity.setType(ResourceType.SITE.value());
             permissionEntity.setPermissionWithin(true);
             description.setPermission(permissionEntity);
         }
         Long permissionKey = queryPermission(makeCode(description.getService(), resource, action));
-        if (permissionKey != null){
+        if (permissionKey != null) {
             update(description.getPermission(), description.getPath(), description.getMethod(), description.getService(), resource, action);
         } else {
             insert(description.getPermission(), description.getPath(), description.getMethod(), description.getService(), resource, action);
@@ -62,9 +68,9 @@ public class PermissionLoader {
 
     private Map<String, Long> queryRoleMap() throws SQLException {
         Map<String, Long> result = new HashMap<>();
-        try(PreparedStatement ps = connection.prepareStatement("SELECT ID, CODE FROM iam_role")){
-            try(ResultSet resultSet = ps.executeQuery()){
-                while (resultSet.next()){
+        try (PreparedStatement ps = connection.prepareStatement("SELECT ID, CODE FROM iam_role")) {
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
                     result.put(resultSet.getString("CODE"), resultSet.getLong("ID"));
                 }
             }
@@ -74,33 +80,33 @@ public class PermissionLoader {
 
     private void insertRolePermission(Long permissionKey, Set<String> roles) throws SQLException {
         roles.add("ADMIN");
-        for (String roleCode : roles){
+        for (String roleCode : roles) {
             Long roleId = roleMap.get(roleCode);
-            if (roleId == null){
+            if (roleId == null) {
                 System.out.println(roleMap);
                 throw new IllegalArgumentException(String.format("role code [%s] not found.", roleCode));
             }
-            try(PreparedStatement ps = connection.prepareStatement("INSERT INTO iam_role_permission (ROLE_ID, PERMISSION_ID) VALUES (?, ?)")) {
+            try (PreparedStatement ps = connection.prepareStatement(getRolePermissionInsertSql())) {
                 ps.setLong(1, roleId);
                 ps.setLong(2, permissionKey);
-                if (ps.executeUpdate() != 1){
+                if (ps.executeUpdate() != 1) {
                     throw new IllegalStateException("update permission result not one.");
                 }
             }
         }
     }
 
-    private String makeCode(String service, String resource, String action){
+    private String makeCode(String service, String resource, String action) {
         String code = resource + "." + action;
-        if (service != null){
+        if (service != null) {
             code = service + "." + code;
         }
         return code;
     }
 
     private void update(PermissionEntity permission, String path, String method, String service, String resource, String action) throws SQLException {
-        try(PreparedStatement ps = connection.prepareStatement("UPDATE iam_permission SET PATH=?, SERVICE_CODE=?, CONTROLLER=?, ACTION=?" +
-                ", METHOD=?, RESOURCE_LEVEL=?, IS_PUBLIC_ACCESS=?, IS_LOGIN_ACCESS=?, IS_WITHIN=? WHERE CODE=?")){
+        try (PreparedStatement ps = connection.prepareStatement("UPDATE iam_permission SET PATH=?, SERVICE_CODE=?, CONTROLLER=?, ACTION=?" +
+                ", METHOD=?, RESOURCE_LEVEL=?, IS_PUBLIC_ACCESS=?, IS_LOGIN_ACCESS=?, IS_WITHIN=? WHERE CODE=?")) {
             ps.setString(1, path);
             ps.setString(2, service);
             ps.setString(3, resource);
@@ -111,15 +117,14 @@ public class PermissionLoader {
             ps.setBoolean(8, permission.isPermissionLogin());
             ps.setBoolean(9, permission.isPermissionWithin());
             ps.setString(10, makeCode(service, resource, action));
-            if (ps.executeUpdate() != 1){
+            if (ps.executeUpdate() != 1) {
                 throw new IllegalStateException("update permission result not one.");
             }
         }
     }
 
     private void insert(PermissionEntity permission, String path, String method, String service, String resource, String action) throws SQLException {
-        try(PreparedStatement ps = connection.prepareStatement("INSERT INTO iam_permission (CODE, PATH, SERVICE_CODE, CONTROLLER, ACTION, METHOD," +
-                " RESOURCE_LEVEL, IS_PUBLIC_ACCESS, IS_LOGIN_ACCESS, IS_WITHIN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")){
+        try (PreparedStatement ps = connection.prepareStatement(getPermissionInsertSql())) {
             ps.setString(1, makeCode(service, resource, action));
             ps.setString(2, path);
             ps.setString(3, service);
@@ -130,17 +135,51 @@ public class PermissionLoader {
             ps.setBoolean(8, permission.isPermissionPublic());
             ps.setBoolean(9, permission.isPermissionLogin());
             ps.setBoolean(10, permission.isPermissionWithin());
-            if (ps.executeUpdate() != 1){
+            if (ps.executeUpdate() != 1) {
                 throw new IllegalStateException("update permission result not one.");
             }
         }
     }
 
+    private String getRolePermissionInsertSql() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO iam_role_permission (");
+        if (liquibaseHelper.isSupportSequence()) {
+            sb.append("ID, ");
+        }
+        sb.append("ROLE_ID, PERMISSION_ID) VALUES (");
+        if (liquibaseHelper.isOracle() || liquibaseHelper.isHana()) {
+            sb.append("IAM_ROLE_PERMISSION_S.nextval, ");
+        }
+        if (liquibaseHelper.isPostgresql()) {
+            sb.append("nextval('IAM_ROLE_PERMISSION_S'), ");
+        }
+        sb.append("?, ?)");
+        return sb.toString();
+    }
+
+    private String getPermissionInsertSql() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO iam_permission (");
+        if (liquibaseHelper.isSupportSequence()) {
+            sb.append("ID, ");
+        }
+        sb.append("CODE, PATH, SERVICE_CODE, CONTROLLER, ACTION, METHOD,RESOURCE_LEVEL, IS_PUBLIC_ACCESS, IS_LOGIN_ACCESS, IS_WITHIN) VALUES (");
+        if (liquibaseHelper.isOracle() || liquibaseHelper.isHana()) {
+            sb.append("IAM_PERMISSION_S.nextval, ");
+        }
+        if (liquibaseHelper.isPostgresql()) {
+            sb.append("nextval('IAM_PERMISSION_S'), ");
+        }
+        sb.append("?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        return sb.toString();
+    }
+
     private Long queryPermission(String code) throws SQLException {
-        try(PreparedStatement ps = connection.prepareStatement("SELECT ID FROM iam_permission WHERE CODE=?")){
+        try (PreparedStatement ps = connection.prepareStatement("SELECT ID FROM iam_permission WHERE CODE=?")) {
             ps.setString(1, code);
-            try(ResultSet resultSet = ps.executeQuery()){
-                if(resultSet.first()){
+            try (ResultSet resultSet = ps.executeQuery()) {
+                if (resultSet.next()) {
                     return resultSet.getLong("ID");
                 }
                 return null;
@@ -163,7 +202,7 @@ public class PermissionLoader {
         for (int i = 0; i < len; i++) {
             char c = param.charAt(i);
             if (Character.isUpperCase(c)) {
-                if (i > 0){
+                if (i > 0) {
                     sb.append(UNDERLINE);
                 }
                 sb.append(Character.toLowerCase(c));
