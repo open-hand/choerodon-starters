05fc4f0d7aeafac9f49defb09d30faea13ea0764
hailuo.liu <hailuo.liu@choerodon.io>
2019-07-02 10:53:56 +0800

    [IMP] 实现choerodon专用的websocket库基础

---
+++ choerodon-starter-websocket/src/main/java/io/choerodon/websocket/v2/helper/TokenChecker.java
@@ -0,0 +1,8 @@
+package io.choerodon.websocket.v2.helper;
+
+/**
+ * Created by hailuo.liu@choerodon.io on 2019/7/2.
+ */
+public interface TokenChecker {
+    public boolean check(String Token);
+}
