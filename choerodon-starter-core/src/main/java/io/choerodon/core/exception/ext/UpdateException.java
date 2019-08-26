a391445a80510f80e0af0af699b7a3cd436f2116
azengqiang <qiang.zeng@hand-china.com>
2019-08-26 11:49:15 +0800

    [FIX] 修改单词拼写错误

--- choerodon-starter-core/src/main/java/io/choerodon/core/exception/ext/UpdateException.java
+++ choerodon-starter-core/src/main/java/io/choerodon/core/exception/ext/UpdateException.java
@@ -8,20 +8,20 @@ import io.choerodon.core.exception.CommonException;
  * @author superlee
  * @since 2019-07-10
  */
-public class UpdateExcetion extends CommonException {
-    public UpdateExcetion(String code, Object... parameters) {
+public class UpdateException extends CommonException {
+    public UpdateException(String code, Object... parameters) {
         super(code, parameters);
     }
 
-    public UpdateExcetion(String code, Throwable cause, Object... parameters) {
+    public UpdateException(String code, Throwable cause, Object... parameters) {
         super(code, cause, parameters);
     }
 
-    public UpdateExcetion(String code, Throwable cause) {
+    public UpdateException(String code, Throwable cause) {
         super(code, cause);
     }
 
-    public UpdateExcetion(Throwable cause, Object... parameters) {
+    public UpdateException(Throwable cause, Object... parameters) {
         super(cause, parameters);
     }
 }
