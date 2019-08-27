e683956f6a48d5c0508f905ccb9f40c7b78426f7
xausky <xausky@163.com>
2019-08-27 14:25:10 +0800

    [FIX] 删除无效的MapKey优化内存占用，方法同步解决并发问题

--- choerodon-starter-websocket/src/main/java/io/choerodon/websocket/send/relationship/DefaultBrokerKeySessionMapper.java
+++ choerodon-starter-websocket/src/main/java/io/choerodon/websocket/send/relationship/DefaultBrokerKeySessionMapper.java
@@ -3,22 +3,17 @@ package io.choerodon.websocket.send.relationship;
 import io.choerodon.websocket.send.BrokerManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.util.StringUtils;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class DefaultBrokerKeySessionMapper implements BrokerKeySessionMapper {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BrokerKeySessionMapper.class);
-    private Map<String, Set<WebSocketSession>> keySessionsMap = new ConcurrentHashMap<>();
-    private Map<WebSocketSession, Set<String>> sessionKeysMap = new ConcurrentHashMap<>();
+    private Map<String, Set<WebSocketSession>> keySessionsMap = new HashMap<>();
+    private Map<WebSocketSession, Set<String>> sessionKeysMap = new HashMap<>();
     private StringRedisTemplate redisTemplate;
     private BrokerManager brokerManager;
 
@@ -28,7 +23,7 @@ public class DefaultBrokerKeySessionMapper implements BrokerKeySessionMapper {
     }
 
     @Override
-    public void subscribe(String messageKey, WebSocketSession session) {
+    public synchronized void subscribe(String messageKey, WebSocketSession session) {
         if (StringUtils.isEmpty(messageKey)) {
             return;
         }
@@ -43,12 +38,13 @@ public class DefaultBrokerKeySessionMapper implements BrokerKeySessionMapper {
     }
 
     @Override
-    public void unsubscribeAll(WebSocketSession session) {
+    public synchronized void unsubscribeAll(WebSocketSession session) {
         Set<String> sessionKeys = sessionKeysMap.computeIfAbsent(session, k -> new HashSet<>());
         for(String messageKey: sessionKeys){
             Set<WebSocketSession> keySessions = keySessionsMap.computeIfAbsent(messageKey, k -> new HashSet<>());
             keySessions.remove(session);
             if(keySessions.isEmpty()){
+                keySessionsMap.remove(messageKey);
                 redisTemplate.opsForSet().remove(brokerManager.getBrokerKeyMapKey(), messageKey);
             }
         }
@@ -56,12 +52,16 @@ public class DefaultBrokerKeySessionMapper implements BrokerKeySessionMapper {
     }
 
     @Override
-    public void unsubscribe(WebSocketSession session, String messageKey) {
+    public synchronized void unsubscribe(WebSocketSession session, String messageKey) {
         Set<WebSocketSession> keySessions = keySessionsMap.computeIfAbsent(messageKey, k -> new HashSet<>());
         keySessions.remove(session);
         Set<String> sessionKeys = sessionKeysMap.computeIfAbsent(session, k -> new HashSet<>());
         sessionKeys.remove(messageKey);
-        if(keySessions.isEmpty()){
+        if (sessionKeys.isEmpty()){
+            sessionKeysMap.remove(session);
+        }
+        if (keySessions.isEmpty()){
+            keySessionsMap.remove(messageKey);
             redisTemplate.opsForSet().remove(brokerManager.getBrokerKeyMapKey(), messageKey);
         }
     }
