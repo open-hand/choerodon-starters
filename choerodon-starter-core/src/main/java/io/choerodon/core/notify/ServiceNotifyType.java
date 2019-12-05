821b074dbd67539ebffbca68e7d0a38f4ad1722a
scp <changping.shi@hand-china.com>
2019-12-05 18:39:32 +0800

    [IMP] 修改notify注解

---
+++ choerodon-starter-core/src/main/java/io/choerodon/core/notify/ServiceNotifyType.java
@@ -0,0 +1,40 @@
+package io.choerodon.core.notify;
+
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * User: Mr.Wang
+ * Date: 2019/12/3
+ * 消息通知的类型
+ */
+public enum ServiceNotifyType {
+    AGILE_NOTIFY("agile"),
+    DEVOPS_NOTIFY("devops"),
+    DEFAULT_NOTIFY("default");
+
+    private String typeName;
+
+    ServiceNotifyType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    /**
+     * 根据类型的名称，返回类型的枚举实例。
+     *
+     * @param typeName 类型名称
+     */
+    public static NotifyType fromTypeName(String typeName) {
+        for (NotifyType type : NotifyType.values()) {
+            if (type.getValue().equals(typeName)) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+    public String getTypeName() {
+        return this.typeName;
+    }
+
+
+}
