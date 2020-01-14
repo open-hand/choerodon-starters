9eca551ce0ed709e61f08bddf7c8370805d2a952
zmf <1984654893@qq.com>
2020-01-14 17:04:40 +0800

    [ADD] add api about access requests in gitlab4j

---
+++ choerodon-gitlab4j-api/src/main/java/org/gitlab4j/api/models/AccessRequest.java
@@ -0,0 +1,103 @@
+package org.gitlab4j.api.models;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * @author zmf
+ * @since 19-12-27
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AccessRequest {
+    private String avatarUrl;
+    private Date createdAt;
+    private String email;
+    private Integer id;
+    private String name;
+    private String state;
+    private String username;
+    private String webUrl;
+    private Date requestedAt;
+    private AccessLevel accessLevel;
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+
+    public Date getRequestedAt() {
+        return requestedAt;
+    }
+
+    public void setRequestedAt(Date requestedAt) {
+        this.requestedAt = requestedAt;
+    }
+
+    public AccessLevel getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(AccessLevel accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+}
