450715b47447c3f7b92682c95026772e54dfd3aa
scp <changping.shi@hand-china.com>
2020-03-19 14:48:34 +0800

    [IMP] 修改webhook Json对象

--- choerodon-starter-core/src/main/java/io/choerodon/core/notify/WebHookSendDTO.java
+++ choerodon-starter-core/src/main/java/io/choerodon/core/notify/WebHookSendDTO.java
@@ -31,6 +31,40 @@ public class WebHookSendDTO {
      */
     private Date createdAt;
 
+    /**
+     * 执行者
+     */
+    private User user;
+
+    public static class User {
+        private String loginName;
+        private String userName;
+
+        public String getLoginName() {
+            return loginName;
+        }
+
+        public void setLoginName(String loginName) {
+            this.loginName = loginName;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public String getObjectKind() {
         return objectKind;
     }
