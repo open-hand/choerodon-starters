f86968969374b9a29618a89f4599e854119d4a4e
scp <changping.shi@hand-china.com>
2020-05-13 14:12:12 +0800

    [ADD] 添加消息发动公共类

---
+++ choerodon-starter-core/src/main/java/io/choerodon/core/enums/MessageAdditionalType.java
@@ -0,0 +1,35 @@
+package io.choerodon.core.enums;
+
+/**
+ * @author scp
+ * @date 2020/5/13
+ * @description
+ */
+public enum MessageAdditionalType {
+    PARAM_PROJECT_ID("projectId"),
+    PARAM_ENV_ID("envId");
+
+    private String typeName;
+
+    MessageAdditionalType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    /**
+     * 根据类型的名称，返回类型的枚举实例。
+     *
+     * @param typeName 类型名称
+     */
+    public static MessageAdditionalType fromTypeName(String typeName) {
+        for (MessageAdditionalType type : MessageAdditionalType.values()) {
+            if (type.getTypeName().equals(typeName)) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+    public String getTypeName() {
+        return this.typeName;
+    }
+}
