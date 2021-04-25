6c1aac0ef831af2712561e8a68a0b6e884c9721d
18986 <changping.shi@hand-chian.com>
2021-04-25 10:35:59 +0800

    [IMP] 优化类注入

--- choerodon-starter-fragment/src/main/java/io/choerodon/fragment/autoconfigure/FragmentC7nAutoConfig.java
+++ choerodon-starter-fragment/src/main/java/io/choerodon/fragment/autoconfigure/FragmentC7nAutoConfig.java
@@ -4,6 +4,7 @@ import org.hzero.fragment.service.FragmentService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import io.choerodon.fragment.service.impl.FragmentC7nServiceImpl;
 
@@ -17,6 +18,7 @@ import io.choerodon.fragment.service.impl.FragmentC7nServiceImpl;
 public class FragmentC7nAutoConfig {
 
     @Bean
+    @Primary
     public FragmentService transferService() {
         return new FragmentC7nServiceImpl();
     }
