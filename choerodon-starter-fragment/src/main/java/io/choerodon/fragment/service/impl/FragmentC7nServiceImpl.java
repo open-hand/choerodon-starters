861b5f07005db2157e8ca3f8ce183e5349ed8cb3
18986 <changping.shi@hand-chian.com>
2021-05-06 10:42:28 +0800

    [IMP] 修改文件上传

--- choerodon-starter-fragment/src/main/java/io/choerodon/fragment/service/impl/FragmentC7nServiceImpl.java
+++ choerodon-starter-fragment/src/main/java/io/choerodon/fragment/service/impl/FragmentC7nServiceImpl.java
@@ -1,11 +1,13 @@
 package io.choerodon.fragment.service.impl;
 
-import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.hzero.core.base.BaseConstants;
 import org.hzero.core.util.Pair;
@@ -16,9 +18,7 @@ import org.hzero.fragment.service.impl.FragmentServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.choerodon.core.convertor.ApplicationContextHelper;
@@ -33,6 +33,9 @@ import io.choerodon.core.exception.CommonException;
 public class FragmentC7nServiceImpl extends FragmentServiceImpl implements FragmentService {
 
     private static final Logger logger = LoggerFactory.getLogger(FragmentC7nServiceImpl.class);
+
+    @Autowired
+    private FragmentConfig fragmentConfig;
 
     @Override
     public String combineUpload(String guid, Long tenantId, String filename, Map<String, String> params) {
@@ -55,11 +58,36 @@ public class FragmentC7nServiceImpl extends FragmentServiceImpl implements Fragm
             return url;
         } catch (IOException e) {
             logger.error("exception:", e);
-            return null;
-        } finally {
-            // 删除文件
             deleteFile(new File(filePath));
+            return null;
+        } catch (Exception e) {
+            logger.error("exception:", e);
+            deleteFile(new File(filePath));
+            throw e;
         }
+    }
+
+    @Override
+    public void upload(MultipartFile file, Integer chunk, String guid) {
+        try (InputStream inputStream = file.getInputStream()) {
+            logger.info("------------------fileName:{},chunk:{}", file.getName(), chunk);
+            if (chunk == null) {
+                chunk = 0;
+            }
+            logger.info("==================fileName:{},chunk:{}", file.getName(), chunk);
+            String filePath = getRootPath() + File.separator + TEMP + File.separator + guid + File.separator + chunk;
+            FileUtils.copyInputStreamToFile(inputStream, new File(filePath));
+        } catch (Exception e) {
+            throw new CommonException(BaseConstants.ErrorCode.ERROR, e);
+        }
+    }
+
+    private String getRootPath() {
+        String rootPath = fragmentConfig.getRootPath();
+        if (StringUtils.isBlank(rootPath)) {
+            return ROOT;
+        }
+        return rootPath;
     }
 
     /**
