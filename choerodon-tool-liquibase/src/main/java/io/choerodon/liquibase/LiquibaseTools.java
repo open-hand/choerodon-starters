77680eb288e02663cc8a9148b3afd686544fa647
王灏 <hao.wang08@hand-china.com>
2021-08-26 09:44:47 +0800

    [imp] 修改初始化脚本

--- choerodon-tool-liquibase/src/main/java/io/choerodon/liquibase/LiquibaseTools.java
+++ choerodon-tool-liquibase/src/main/java/io/choerodon/liquibase/LiquibaseTools.java
@@ -1,16 +1,12 @@
 package io.choerodon.liquibase;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by xausky on 4/6/17.
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.hzero.helper.*"}, basePackageClasses = io.choerodon.liquibase.StartupRunner.class)
-@MapperScan(basePackages = "org.hzero.installer.mapper")
 public class LiquibaseTools {
 
 
