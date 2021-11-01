a44cb1a26b669532bd11ca1810718d3e48a6c625
王灏 <hao.wang08@hand-china.com>
2021-11-01 09:51:22 +0800

    [ADD] 修改限流配置

--- choerodon-starter-limit/src/main/java/io/choerodon/limiter/RedisGitlabPermitsProperties.java
+++ choerodon-starter-limit/src/main/java/io/choerodon/limiter/RedisGitlabPermitsProperties.java
@@ -9,11 +9,11 @@ public class RedisGitlabPermitsProperties {
     /**
      * 每秒生成的令牌数
      */
-    private double permitsPerSecond = 20.0;
+    private double permitsPerSecond = 100.0;
     /**
      * 存储未使用的令牌多少秒，桶中的最大令牌数 = permitsPerSecond * maxBurstSeconds
      */
-    private double maxBurstSeconds = 5.0;
+    private double maxBurstSeconds = 1.0;
     /**
      * 令牌的失效时间(s)
       */
