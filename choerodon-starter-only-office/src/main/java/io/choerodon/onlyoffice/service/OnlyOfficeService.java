d5f60ca7ce2674f56747706367cebb70465a2b21
wangxiang <xiangwang04@hand-china.com>
2022-05-09 11:23:25 +0800

    [FIX] 增加参数的接收

--- choerodon-starter-only-office/src/main/java/io/choerodon/onlyoffice/service/OnlyOfficeService.java
+++ choerodon-starter-only-office/src/main/java/io/choerodon/onlyoffice/service/OnlyOfficeService.java
@@ -1,10 +1,12 @@
 package io.choerodon.onlyoffice.service;
 
 import com.alibaba.fastjson.JSONObject;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 /**
  * Created by wangxiang on 2022/5/6
  */
 public interface OnlyOfficeService {
-    void saveFile(JSONObject obj);
+    void saveFile(JSONObject obj) throws IOException;
 }
---
+++ choerodon-starter-only-office/src/main/java/io/choerodon/onlyoffice/vo/DocumentEditCallback.java
@@ -0,0 +1,62 @@
+package io.choerodon.onlyoffice.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * Created by wangxiang on 2022/5/6
+ */
+public class DocumentEditCallback {
+    @ApiModelProperty("文件的后缀")
+    private String fileType;
+
+    @ApiModelProperty("fileId")
+    private String key;
+
+    @ApiModelProperty("新文件的下载地址")
+    private String url;
+
+    @ApiModelProperty("文件的名称")
+    private String title;
+
+    private Integer status;
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
